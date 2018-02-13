e0cf0d0b54616e2b63d133ddb307f1e2ddda3fa7
pboymt <pboymt@foxmail.com>
2018-02-13 09:28:59 +0800

    更新数据

--- static/index.js
+++ static/index.js
@@ -16,7 +16,7 @@ if (!String.prototype.padStart) {
 
 function formatTime(time = 0) {
     const date = new Date(time);
-    return `${date.getFullYear()}年${date.getMonth().toString().padStart(2,'0')}月${date.getDate().toString().padStart(2,'0')}日 ${date.getHours().toString().padStart(2,'0')}:${date.getMinutes().toString().padStart(2,'0')}:${date.getSeconds().toString().padStart(2,'0')}`
+    return `${date.getFullYear()}年${(date.getMonth()+1).toString().padStart(2,'0')}月${date.getDate().toString().padStart(2,'0')}日 ${date.getHours().toString().padStart(2,'0')}:${date.getMinutes().toString().padStart(2,'0')}:${date.getSeconds().toString().padStart(2,'0')}`
 }
 $(() => {
     console.log('?');
